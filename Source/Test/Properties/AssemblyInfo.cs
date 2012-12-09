4568f21f659778fa700a7ead90adcfb28725d5a6
void <git.email.address@googlemail.com>
2012-12-09 21:03:10 +0100

    Cosmetics

---
+++ Source/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Panzer Kontrol - Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2519bfa3-554b-417e-9ce1-1fea4b75758d")]
+
+
+[assembly: AssemblyVersion("0.0.0.0")]
