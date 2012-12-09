cf0f7a8c699b5194dd6c5c6f34930e492f99de1b
void <git.email.address@googlemail.com>
2012-12-09 16:24:33 +0100

    Added unit upgrades

---
+++ Source/Common/UnitUpgrade.cs
@@ -0,0 +1,17 @@
+﻿namespace PanzerKontrol
+{
+	class UnitUpgrade
+	{
+		// The name of the upgrade.
+		public string Name { get; set; }
+
+		// A description of what the upgrade does.
+		public string Description { get; set; }
+
+		// The number of points this upgrade costs.
+		public int Price { get; set; }
+
+		// Additive bonuses to the unit that is upgraded.
+		public UnitStats Bonus { get; set; }
+	}
+}
